db9969cd79c5283cc8709e2946224f4126109eb7
Jake Weinstein <jake@aospa.co>
2020-06-02 20:56:08 -0400

    sm8350-common: Add init script to set properties per variant
     * Adapted from LineageOS.
    
    Change-Id: Ief5d98e41a5808d057096b25a6e743e608660e12
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- common.mk
+++ common.mk
@@ -343,6 +343,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
+# Init
+PRODUCT_PACKAGES += \
+    libinit_sm8350
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
